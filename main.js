31eb1f872ad6627ae9f8fe931ddd729e35ded8a6
jasmineavie <jasminemichaelavie@gmail.com>
2023-12-15 19:12:43 -0600

    12/15

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-const 
+const BREEDS_URL = ("https://dog.ceo/api/breeds/list/all")
+
+
+fetch(BREEDS_URL)
+.then(response => {
+   return response.json()
+}
+
+)
+.then(data => {
+    console.log(data.message)
+    const breedsObject = data.message
+    const breedsKeys = Object.keys(breedsObject)
+    console.log(breedsKeys)
+})
+
+.catch(err => {
+    console.log(`error is ${err}`)
+} )
