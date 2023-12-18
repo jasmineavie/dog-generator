16fdfe3abf3dd263d3b330763399000ad2c505c6
jasmineavie <jasminemichaelavie@gmail.com>
2023-12-18 17:18:42 -0600

    12/18

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
 const BREEDS_URL = ("https://dog.ceo/api/breeds/list/all")
+const select = document.querySelector(".breeds")
 
 
 fetch(BREEDS_URL)
 .then(response => {
-   return response.json()
+   return response.json();
 }
 
 )
 .then(data => {
-    console.log(data.message)
-    const breedsObject = data.message
+
+    const breedsObject = data.message;
     const breedsKeys = Object.keys(breedsObject)
-    console.log(breedsKeys)
+
+
+
+    for(let i = 0; i < breedsKeys.length; i++) {
+     const option = document.createElement("option")
+     option.value = breedsKeys[i]
+     option.innerText = breedsKeys[i]
+    select.appendChild(option)
+
+
+    }
 })
 
-.catch(err => {
+select.addEventListener("change", )
+
+
+/*.then(data => {
+    const breedKeys = Object.keys(data.message)
+}) */
+
+/* .catch(err => {
     console.log(`error is ${err}`)
-} )
+} ) */
